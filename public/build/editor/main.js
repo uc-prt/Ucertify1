c37d7eb8c68ea32d1f7138ca9549e15af39e4056
uc-prk <prabhat.kumar@ucertify.com>
2021-07-08 08:01:04 +0530

    updated css

--- public/build/editor/main.js
+++ public/build/editor/main.js
@@ -1,2 +1,4 @@
-export{ae as default}from"./main-daecf310.js";
+
+(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
+export { al as default } from './main-bce9756a.js';
 //# sourceMappingURL=main.js.map
