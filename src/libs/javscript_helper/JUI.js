8462c8b8554163cf4faf1ba78fa6b28061685388
uc-prk <prabhat.kumar@ucertify.com>
2021-07-08 10:47:56 +0530

    updated code

--- src/libs/javscript_helper/JUI.js
+++ src/libs/javscript_helper/JUI.js
@@ -11,10 +11,11 @@
 export class API {
     constructor(options) {
         this._servers = [
-            'http://localhost/pe-gold3/', 
-            'https://www.ucertify.com/', 
-            'https://www.jigyaasa.info/',
-            'http://172.10.195.203/pe-gold3/',
+            location.origin + (location.host == "localhost" ? '/pe-gold3/' : "/");
+            // 'http://localhost/pe-gold3/', 
+            // 'https://www.ucertify.com/', 
+            // 'https://www.jigyaasa.info/',
+            // 'http://172.10.195.203/pe-gold3/',
         ];
         this._REMOTE_API_URL = this._servers[1] + 'pe-api/1/index.php';
         this._client = {
