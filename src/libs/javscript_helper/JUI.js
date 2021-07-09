c8ed5a017fb13c34778e4a98085621ee9c1e04cb
uc-prk <prabhat.kumar@ucertify.com>
2021-07-09 10:55:07 +0530

    updated jui files

--- src/libs/javscript_helper/JUI.js
+++ src/libs/javscript_helper/JUI.js
@@ -11,11 +11,10 @@
 export class API {
     constructor(options) {
         this._servers = [
-            location.origin + (location.host == "localhost" ? '/pe-gold3/' : "/")
-            // 'https://localhost/pe-gold3/', 
-            // 'https://www.ucertify.com/', 
-            // 'https://www.jigyaasa.info/',
-            // 'https://172.10.195.203/pe-gold3/',
+            'https://localhost/pe-gold3/', 
+            'https://www.ucertify.com/', 
+            'https://www.jigyaasa.info/',
+            'https://172.10.195.203/pe-gold3/',
         ];
         this._REMOTE_API_URL = this._servers[1] + 'pe-api/1/index.php';
         //@Prabhat: Why is this here. Need to remove this.
