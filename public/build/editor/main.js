c2169e8f146cca02740ba05d4627b5e1118f5bcc
uc-prk <prabhat.kumar@ucertify.com>
2021-07-11 09:12:59 +0530

    updated chunks files

---
+++ public/build/editor/main.js
@@ -0,0 +1,4 @@
+
+(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
+export { al as default } from './main-b83e0690.js';
+//# sourceMappingURL=main.js.map
