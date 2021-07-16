162d9a7d73b883dc5201a9e16ab3900694ff84a9
Sundaram Tripathi <sundaram.tripathi@ucertify.com>
2021-07-16 17:36:51 +0530

    fixed

--- clsSMChoose/ChooseNReorderPreview.svelte
+++ clsSMChoose/ChooseNReorderPreview.svelte
@@ -395,7 +395,7 @@
     // Return the html and css
     function setInnerHtml(item) { 
         
-        let htmlContent = '<div class="prefix pl-2 position-relative mr-2"'+((state.isSentence == "1")?"nw":"")+'"></div>'+((state.isParagraph == "1")?'<div class="pg_handle">&equiv;</div>':"")+((item.value.charAt(0) == "*")? item.value.slice(1) :item.value)
+        let htmlContent = '<div class="prefix pl-2 mr-2"'+((state.isSentence == "1")?"nw":"")+'"></div>'+((state.isParagraph == "1")?'<div class="pg_handle">&equiv;</div>':"")+((item.value.charAt(0) == "*")? item.value.slice(1) :item.value)
         
         return htmlContent;
 	}
