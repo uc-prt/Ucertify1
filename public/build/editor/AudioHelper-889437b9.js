d983b18d3935b960fb24482e6152703b677b0818
uc-prk <prabhat.kumar@ucertify.com>
2021-07-29 14:15:10 +0530

    updating bundles

--- public/build/editor/AudioHelper-889437b9.js
+++
@@ -1,2 +0,0 @@
-import{S as s,i as t,s as e}from"./main-b1387daf.js";const o={helpVideo:{}};export default class extends s{constructor(s){super(),t(this,s,null,null,e,{})}}export{o as itemHelper};
-//# sourceMappingURL=AudioHelper-889437b9.js.map
--- public/build/editor/External-69e499bc.js
+++
@@ -1,2 +0,0 @@
-import{S as s,i as a,s as t}from"./main-b1387daf.js";export default class extends s{constructor(s){super(),a(this,s,null,null,t,{})}}
-//# sourceMappingURL=External-69e499bc.js.map
