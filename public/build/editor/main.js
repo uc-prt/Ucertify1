27ce67adf86342c88e2eafcbda71eae105780ed0
uc-prk <prabhat.kumar@ucertify.com>
2021-08-01 16:35:34 +0530

    Fixed the FIB Issue

--- public/build/editor/main.js
+++ public/build/editor/main.js
@@ -1,2 +1,4 @@
-export{ag as default}from"./main-8fa5e5da.js";
+
+(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
+export { an as default } from './main-a707bd56.js';
 //# sourceMappingURL=main.js.map
