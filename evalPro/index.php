4394c041103652342fadf9ef624fa8225c4b684e
uc-prk <prabhat.kumar@ucertify.com>
2021-08-02 18:03:59 +0530

    updated code

--- evalPro/index.php
+++ evalPro/index.php
@@ -8,7 +8,6 @@
  * Last updated date : 10-Aug-2020
  */
 $is_mincall = 1;
-header( 'Access-Control-Allow-Origin: *' );
 header( 'Access-Control-Allow-Methods: POST' );
 header( 'Access-Control-Allow-Credentials: true' );
 header( 'Access-Control-Allow-Headers: X-Requested-With, Content-Type' );
