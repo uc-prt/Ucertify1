92882b99842b8d5c8b928d91a1ae893dea62ed79
uc-prk <prabhat.kumar@ucertify.com>
2021-08-12 10:27:46 +0530

    deleted bundles

--- public/build/editor/fillHelper-3a3286e6.js
+++
@@ -1,2 +0,0 @@
-import{S as s,i as t,s as e}from"./main-a536cf38.js";const o={helpVideo:{}};export default class extends s{constructor(s){super(),t(this,s,null,null,e,{})}}export{o as itemHelper};
-//# sourceMappingURL=fillHelper-3a3286e6.js.map
