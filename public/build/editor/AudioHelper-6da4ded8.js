146e618901bc09b64856667c11da02642f2033ef
uc-prk <prabhat.kumar@ucertify.com>
2021-08-13 18:23:35 +0530

    updating editor bundles

---
+++ public/build/editor/AudioHelper-6da4ded8.js
@@ -0,0 +1,2 @@
+import{S as s,i as t,s as e}from"./main-443267d2.js";const o={helpVideo:{}};export default class extends s{constructor(s){super(),t(this,s,null,null,e,{})}}export{o as itemHelper};
+//# sourceMappingURL=AudioHelper-6da4ded8.js.map
--- public/build/editor/main.js
+++ public/build/editor/main.js
@@ -1,2 +1,2 @@
-export{ag as default}from"./main-cddbb274.js";
+export{ag as default}from"./main-443267d2.js";
 //# sourceMappingURL=main.js.map
