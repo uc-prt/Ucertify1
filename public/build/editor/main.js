d44078db49de15426fc7f7745ca00a653eee85a9
uc-prk <prabhat.kumar@ucertify.com>
2021-08-18 11:32:30 +0530

    updating bundles

--- public/build/editor/main.js
+++ public/build/editor/main.js
@@ -1,4 +1,2 @@
-
-(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
-export { an as default } from './main-5b31e398.js';
+export{ag as default}from"./main-d0c67d72.js";
 //# sourceMappingURL=main.js.map
