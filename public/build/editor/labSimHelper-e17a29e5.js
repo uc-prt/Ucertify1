c5fd54ada00627f498ca544db2889d6f0bbd8859
uc-prk <prabhat.kumar@ucertify.com>
2021-08-20 16:21:47 +0530

    updated code for the player issue

--- public/build/editor/labSimHelper-e17a29e5.js
+++
@@ -1,2 +0,0 @@
-import{S as e,i as c,s}from"./main-e4b52a97.js";const t={verticalView:!0,getXmlFunc:"generateXML",helpVideo:"//player.vimeo.com/external/289408306.hd.mp4?s=4d09ddeec5780a1654c2cb09379ccf17c749e1c4"};export default class extends e{constructor(e){super(),c(this,e,null,null,s,{})}}export{t as itemHelper};
-//# sourceMappingURL=labSimHelper-e17a29e5.js.map
--- public/build/editor/main.js
+++ public/build/editor/main.js
@@ -1,2 +1,4 @@
-export{ag as default}from"./main-e4b52a97.js";
+
+(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
+export { an as default } from './main-854b7bd4.js';
 //# sourceMappingURL=main.js.map
