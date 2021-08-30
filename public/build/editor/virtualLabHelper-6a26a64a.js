6e390a2fa4c3b21b4225924e5a51f8627f279497
uc-prk <prabhat.kumar@ucertify.com>
2021-08-30 19:38:01 +0530

    updated code

---
+++ public/build/editor/virtualLabHelper-6a26a64a.js
@@ -0,0 +1,2 @@
+import{S as s,i as a,s as t}from"./main-9ba10aee.js";const e={isManualGrade(s){if(s.match(/<smxml\s.*?autograde="1"|<smxml\s.*?autograde='1'/gim))return!0}};export default class extends s{constructor(s){super(),a(this,s,null,null,t,{})}}export{e as itemHelper};
+//# sourceMappingURL=virtualLabHelper-6a26a64a.js.map
