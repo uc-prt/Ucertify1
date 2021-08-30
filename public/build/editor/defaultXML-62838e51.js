4bdb6e381cbd732c41a59dcfb2bf67a9511a757f
uc-prk <prabhat.kumar@ucertify.com>
2021-08-30 13:31:11 +0530

    updated code

--- public/build/editor/defaultXML-62838e51.js
+++
@@ -1,2 +0,0 @@
-import{S as e,i as s,s as t}from"./main-756763a4.js";function a(e){return{"editor_item_4.xml":'<smxml type="5" name="Essay"><default type="0">\x3c!--[CDATA[]]--\x3e</default></smxml>',sample:'<smxml type="5" name="Essay"><default type="0">\x3c!--[CDATA[]]--\x3e</default></smxml>'}[e]}export default class extends e{constructor(e){super(),s(this,e,null,null,t,{})}}export{a as getDefaultXMl};
-//# sourceMappingURL=defaultXML-62838e51.js.map
