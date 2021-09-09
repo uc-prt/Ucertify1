c38d868958972e3818444d844784224bfb503063
uc-prk <prabhat.kumar@ucertify.com>
2021-09-09 15:05:49 +0530

    content setting issue resolved

--- public/build/editor/main.js
+++ public/build/editor/main.js
@@ -1,2 +1,4 @@
-export{ag as default}from"./main-dc1d2975.js";
+
+(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35730/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
+export { an as default } from './main-50717271.js';
 //# sourceMappingURL=main.js.map
