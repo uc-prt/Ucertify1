903625cb4a32ec26d6638d766278bf7dc68b7643
uc-prk <prabhat.kumar@ucertify.com>
2021-09-14 13:01:09 +0530

    updating editor bundles

--- public/build/editor/insightHelper-2503bfb2.js
+++
@@ -1,2 +0,0 @@
-import{S as e,i as a,s}from"./main-1f82e079.js";const t={verticalView:!0,getXmlFunc:"generateXML",helpVideo:"//player.vimeo.com/external/289408469.hd.mp4?s=a7bfff633372e92acdc99c3b9e2e10460ea4ab5c"};export default class extends e{constructor(e){super(),a(this,e,null,null,s,{})}}export{t as itemHelper};
-//# sourceMappingURL=insightHelper-2503bfb2.js.map
--- public/build/editor/main.js
+++ public/build/editor/main.js
@@ -1,2 +1,2 @@
-export{ag as default}from"./main-1f82e079.js";
+export{ag as default}from"./main-62763344.js";
 //# sourceMappingURL=main.js.map
