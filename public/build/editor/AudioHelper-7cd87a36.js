7b41537cff1d89ae46ac26005f3f41b194e91739
uc-prk <prabhat.kumar@ucertify.com>
2021-09-20 14:07:02 +0530

    updating bundles

--- public/build/editor/AudioHelper-7cd87a36.js
+++
@@ -1,2 +0,0 @@
-import{S as s,i as e,s as t}from"./main-e7b82395.js";const o={helpVideo:{}};export default class extends s{constructor(s){super(),e(this,s,null,null,t,{})}}export{o as itemHelper};
-//# sourceMappingURL=AudioHelper-7cd87a36.js.map
--- public/build/editor/main.js
+++ public/build/editor/main.js
@@ -1,2 +1,2 @@
-export{ag as default}from"./main-e7b82395.js";
+export{ag as default}from"./main-e09bd4a8.js";
 //# sourceMappingURL=main.js.map
