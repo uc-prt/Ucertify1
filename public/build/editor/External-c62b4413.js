6e2a0456733afb0fb3bb147d1feb61142de29dbd
uc-prk <prabhat.kumar@ucertify.com>
2021-09-20 14:41:06 +0530

    updating bundles

--- public/build/editor/External-c62b4413.js
+++
@@ -1,2 +0,0 @@
-import{S as s,i as a,s as t}from"./main-e09bd4a8.js";export default class extends s{constructor(s){super(),a(this,s,null,null,t,{})}}
-//# sourceMappingURL=External-c62b4413.js.map
--- public/build/editor/main.js
+++ public/build/editor/main.js
@@ -1,2 +1,2 @@
-export{ag as default}from"./main-e09bd4a8.js";
+export{ag as default}from"./main-21251e08.js";
 //# sourceMappingURL=main.js.map
