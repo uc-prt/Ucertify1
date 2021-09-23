dfe38e455fb9681e04faacba3cb9c309d2e2a396
uc-prk <prabhat.kumar@ucertify.com>
2021-09-23 23:24:54 +0530

    updated code

---
+++ public/build/editor/AlignMatchHelper-9bc678bc.js
@@ -0,0 +1,2 @@
+import{S as e,i as a,s as c}from"./main-eaf51642.js";const s={helpVideo:{0:"//player.vimeo.com/external/289408724.hd.mp4?s=3edaf3922f2c5b983cfc0fcd71594a060603e386",1:"//player.vimeo.com/external/289408724.hd.mp4?s=3edaf3922f2c5b983cfc0fcd71594a060603e386"}};export default class extends e{constructor(e){super(),a(this,e,null,null,c,{})}}export{s as itemHelper};
+//# sourceMappingURL=AlignMatchHelper-9bc678bc.js.map
---
+++ public/build/editor/ChooseMultiGridHelper-358d6ce4.js
@@ -0,0 +1,2 @@
+import{S as s,i as e,s as t}from"./main-eaf51642.js";const o={helpVideo:{}};export default class extends s{constructor(s){super(),e(this,s,null,null,t,{})}}export{o as itemHelper};
+//# sourceMappingURL=ChooseMultiGridHelper-358d6ce4.js.map
