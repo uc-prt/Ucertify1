5aa9ff920525ad0d1f24a8e9d189a50877b378b1
uc-prk <prabhat.kumar@ucertify.com>
2021-09-27 10:56:31 +0530

    updating bundles

--- public/build/editor/main.js
+++ public/build/editor/main.js
@@ -1,2 +1,4 @@
-export{af as default}from"./main-b158ccfe.js";
+
+(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35730/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
+export { am as default } from './main-a3b8c24d.js';
 //# sourceMappingURL=main.js.map
