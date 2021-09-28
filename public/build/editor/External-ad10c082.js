8dd555cbb2fe7bf81c1aa6eb381ddfbc367b329a
uc-prk <prabhat.kumar@ucertify.com>
2021-09-28 15:49:59 +0530

    updated code

--- public/build/editor/External-ad10c082.js
+++
@@ -1,2 +0,0 @@
-import{S as s,i as a,s as t}from"./main-a47bd70c.js";export default class extends s{constructor(s){super(),a(this,s,null,null,t,{})}}
-//# sourceMappingURL=External-ad10c082.js.map
--- public/build/editor/main.js
+++ public/build/editor/main.js
@@ -1,2 +1,2 @@
-export{af as default}from"./main-a47bd70c.js";
+export{af as default}from"./main-5e43927e.js";
 //# sourceMappingURL=main.js.map
