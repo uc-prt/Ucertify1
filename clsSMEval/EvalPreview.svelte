dcfb0a3a236bc9bb9af4d99bad3917aef7b7fab0
uc-prk <prabhat.kumar@ucertify.com>
2021-09-29 17:53:55 +0530

    updated code

--- clsSMEval/EvalPreview.svelte
+++ clsSMEval/EvalPreview.svelte
@@ -8,6 +8,7 @@
 	 *  Package     : svelte_items
 	 *  Created     : 11 March 2021
 	*/
+	import { user } from "../../../store";
 	import { onMount, beforeUpdate } from 'svelte';
 	import { Button, Dialog } from 'svelte-mui/src';
 	import { writable } from 'svelte/store';
@@ -25,7 +26,7 @@
 	export let content_guid;
 	export let sample_input = '';
     let evalpro_url =  'https://www.jigyaasa.info/layout/themes/bootstrap4/svelte_items/evalPro/index.php';
-    let user_guid = '05kmr'; // I will change it later
+    let user_guid = '';
 	let tempGuid;
 	let client = {};
 	let markerFlag = false;
@@ -78,6 +79,9 @@
 
 	const unsubs = hdd.subscribe((items)=> {
 		state = items;
+	})
+	const userunsubs = user.subscribe((user) => {
+		user_guid = user.user_guid;
 	})
 
 	onMount(()=> {
