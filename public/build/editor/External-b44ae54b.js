1521760037fbfe4d6c34ede6901d1ed66b302115
uc-prk <prabhat.kumar@ucertify.com>
2021-10-04 11:24:35 +0530

    updated code

---
+++ public/build/editor/External-b44ae54b.js
@@ -0,0 +1,2 @@
+import{S as s,i as a,s as t}from"./main-664a6022.js";export default class extends s{constructor(s){super(),a(this,s,null,null,t,{})}}
+//# sourceMappingURL=External-b44ae54b.js.map
--- public/build/editor/main.js
+++ public/build/editor/main.js
@@ -1,2 +1,4 @@
-export{ah as default}from"./main-664a6022.js";
+
+(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
+export { ao as default } from './main-2efadb2a.js';
 //# sourceMappingURL=main.js.map
