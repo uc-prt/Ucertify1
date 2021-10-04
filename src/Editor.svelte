96a1de150f3526703b9c349607ea30804aeb8837
Aman Dubey <80241050+uc-amd@users.noreply.github.com>
2021-10-04 17:31:15 +0530

    Update Editor.svelte

--- src/Editor.svelte
+++ src/Editor.svelte
@@ -1449,7 +1449,7 @@ function editorPaneShow(event) {
 		// Render equation
 		activateMathMl(state.stem + state.remediation + state.content, state.variable_button, mathMLRender);
 		state.editorView = 'preview';
-		AH.selectAll('.mce-panel', 'hide', {action: 'hiden'});
+		AH.selectAll('.mce-panel', 'hide', {action: 'hide'});
 	}
 }
 
