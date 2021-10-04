93414c1bdfba665adbbabecbaa53f2f3e9060cd9
uc-prk <prabhat.kumar@ucertify.com>
2021-10-04 11:11:38 +0530

    updating main.js

--- public/build/editor/main.js
+++ public/build/editor/main.js
@@ -1,4 +1,2 @@
-
-(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
-export { ao as default } from './main-1b1c3fb8.js';
+export{ah as default}from"./main-664a6022.js";
 //# sourceMappingURL=main.js.map
