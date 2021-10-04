48e3528e1202269f4e5d04b0b0de9da7e47ee4fe
uc-prk <prabhat.kumar@ucertify.com>
2021-10-04 11:04:21 +0530

    updated code

--- public/build/editor/main.js
+++ public/build/editor/main.js
@@ -1,4 +1,2 @@
-
-(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
-export { ao as default } from './main-0aa88db5.js';
+export{ah as default}from"./main-2d1be4be.js";
 //# sourceMappingURL=main.js.map
