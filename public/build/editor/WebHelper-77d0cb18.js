934fb1cb344278584134813fe0ffd1967f92eaa1
uc-prk <prabhat.kumar@ucertify.com>
2021-10-07 17:44:35 +0530

    updated the code

--- public/build/editor/WebHelper-77d0cb18.js
+++
@@ -1,2 +0,0 @@
-import{S as e,i as s,s as o}from"./main-05e2d249.js";const t={helpVideo:"//player.vimeo.com/external/289408282.hd.mp4?s=6621f3cfb6f6840e10efe63881d844407482b92e"};export default class extends e{constructor(e){super(),s(this,e,null,null,o,{})}}export{t as itemHelper};
-//# sourceMappingURL=WebHelper-77d0cb18.js.map
