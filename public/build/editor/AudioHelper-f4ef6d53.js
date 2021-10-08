9502cda221519de7448e190e6de18a20435d56c5
uc-prk <prabhat.kumar@ucertify.com>
2021-10-08 23:43:19 +0530

    updating bundles

---
+++ public/build/editor/AudioHelper-f4ef6d53.js
@@ -0,0 +1,2 @@
+import{S as s,i as t,s as a}from"./main-b3f3abad.js";const e={helpVideo:{}};export default class extends s{constructor(s){super(),t(this,s,null,null,a,{})}}export{e as itemHelper};
+//# sourceMappingURL=AudioHelper-f4ef6d53.js.map
---
+++ public/build/editor/ChooseMultiGridHelper-aa6a2f4b.js
@@ -0,0 +1,2 @@
+import{S as s,i as t,s as a}from"./main-b3f3abad.js";const e={helpVideo:{}};export default class extends s{constructor(s){super(),t(this,s,null,null,a,{})}}export{e as itemHelper};
+//# sourceMappingURL=ChooseMultiGridHelper-aa6a2f4b.js.map
