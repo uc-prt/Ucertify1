0244c6fd1be2e708fe31c8b9ad623fca09057805
uc-prk <prabhat.kumar@ucertify.com>
2021-10-12 09:37:29 +0530

    updating editor bundles

---
+++ public/build/editor/defaultXML-21f63af4.js
@@ -0,0 +1,2 @@
+import{S as e,i as t,s as a}from"./main-f0420ab1.js";function s(e){return{"editor_item_69.xml":'<SMXML type="22" addhtml="0" name="Web"></SMXML>',sample:'<SMXML type="22" addhtml="0" name="Web"></SMXML>'}[e]}export default class extends e{constructor(e){super(),t(this,e,null,null,a,{})}}export{s as getDefaultXMl};
+//# sourceMappingURL=defaultXML-21f63af4.js.map
--- public/build/editor/defaultXML-a7fa1190.js
+++
@@ -1,2 +0,0 @@
-import{S as e,i as t,s as a}from"./main-ea852077.js";function s(e){return{"editor_item_69.xml":'<SMXML type="22" addhtml="0" name="Web"></SMXML>',sample:'<SMXML type="22" addhtml="0" name="Web"></SMXML>'}[e]}export default class extends e{constructor(e){super(),t(this,e,null,null,a,{})}}export{s as getDefaultXMl};
-//# sourceMappingURL=defaultXML-a7fa1190.js.map
