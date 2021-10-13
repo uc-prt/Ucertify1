6aa9b125d1d4f85675e39ed8bbaa34c8960fbf0b
uc-amd <aman.dubey@ucertify.com>
2021-10-13 17:52:59 +0530

    full screen in player fixed

--- scripts/editorScript/function.js
+++ scripts/editorScript/function.js
@@ -790,3 +790,25 @@ function functionForFullscreen() {
 			? bottomBars.forEach(ele => ele.setAttribute('style', 'display: block')): "";
 	};
 }
+function scrollToTop (duration) {
+    // cancel if already on top
+    if (document.scrollingElement.scrollTop === 0) return;
+
+    const totalScrollDistance = document.scrollingElement.scrollTop;
+    let scrollY = totalScrollDistance, oldTimestamp = null;
+
+    function step (newTimestamp) {
+        if (oldTimestamp !== null) {
+            // if duration is 0 scrollY will be -Infinity
+            scrollY -= totalScrollDistance * (newTimestamp - oldTimestamp) / duration;
+            if (scrollY <= 0) return document.scrollingElement.scrollTop = 0;
+            document.scrollingElement.scrollTop = scrollY;
+        }
+        oldTimestamp = newTimestamp;
+        window.requestAnimationFrame(step);
+    }
+    window.requestAnimationFrame(step);
+}
+function scrollnow(px) {
+	scrollToTop;
+}
