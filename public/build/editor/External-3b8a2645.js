934ff9ce332fd2dc652db0b9eb7195e50fb7d2a8
uc-amd <aman.dubey@ucertify.com>
2021-10-21 11:09:35 +0530

    Bundle updated

---
+++ public/build/editor/External-3b8a2645.js
@@ -0,0 +1,2 @@
+import{S as s,i as t,s as a}from"./main-6e20d34c.js";export default class extends s{constructor(s){super(),t(this,s,null,null,a,{})}}
+//# sourceMappingURL=External-3b8a2645.js.map
--- public/build/editor/choiceMatrixHelper-fd90de52.js
+++
@@ -1,2 +0,0 @@
-import{S as e,i as s,s as d}from"./main-0924c8c7.js";const c={helpVideo:{0:"//player.vimeo.com/external/286997309.hd.mp4?s=c18e03b856b9446dd2d9dc9ff09b05c7994d2d8d"}};export default class extends e{constructor(e){super(),s(this,e,null,null,d,{})}}export{c as itemHelper};
-//# sourceMappingURL=choiceMatrixHelper-fd90de52.js.map
