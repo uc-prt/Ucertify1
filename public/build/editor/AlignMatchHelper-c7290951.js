3c28bdd6755236e9d46003560bfad4568cebaf26
uc-prk <prabhat.kumar@ucertify.com>
2021-10-22 11:14:42 +0530

    updated the code

--- public/build/editor/AlignMatchHelper-c7290951.js
+++
@@ -1,2 +0,0 @@
-import{S as e,i as c,s as a}from"./main-6e20d34c.js";const s={helpVideo:{0:"//player.vimeo.com/external/289408724.hd.mp4?s=3edaf3922f2c5b983cfc0fcd71594a060603e386",1:"//player.vimeo.com/external/289408724.hd.mp4?s=3edaf3922f2c5b983cfc0fcd71594a060603e386"}};export default class extends e{constructor(e){super(),c(this,e,null,null,a,{})}}export{s as itemHelper};
-//# sourceMappingURL=AlignMatchHelper-c7290951.js.map
