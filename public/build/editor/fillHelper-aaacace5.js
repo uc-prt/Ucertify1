e7f73e6dd58b601839d8a26e24a7ba36085e0e8c
uc-prk <prabhat.kumar@ucertify.com>
2021-11-12 10:06:30 +0530

    updated the code

--- public/build/editor/fillHelper-aaacace5.js
+++
@@ -1,2 +0,0 @@
-import{S as s,i as e,s as t}from"./main-fd6e0884.js";const o={helpVideo:{}};export default class extends s{constructor(s){super(),e(this,s,null,null,t,{})}}export{o as itemHelper};
-//# sourceMappingURL=fillHelper-aaacace5.js.map
--- public/build/editor/virtualLabHelper-480fdc55.js
+++
@@ -1,2 +0,0 @@
-import{S as s,i as a,s as t}from"./main-fd6e0884.js";const r={isManualGrade(s){if(s.match(/<smxml\s.*?autograde="1"|<smxml\s.*?autograde='1'/gim))return!0}};export default class extends s{constructor(s){super(),a(this,s,null,null,t,{})}}export{r as itemHelper};
-//# sourceMappingURL=virtualLabHelper-480fdc55.js.map
