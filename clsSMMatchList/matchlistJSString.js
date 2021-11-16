7f11f9c4a6b21df935c960a9c14bfc1b09319672
uc-sdt <sundaram.tripathi@ucertify.com>
2021-11-16 12:05:44 +0530

    Fixed

--- clsSMMatchList/matchlistJSString.js
+++ clsSMMatchList/matchlistJSString.js
@@ -939,8 +939,9 @@ ucMlid.showAllCorrectAns = function(mlid) {
 ucMlid.showAllAns = function(mlid) {
     AH.remove("#lines");
     AH.remove('.correct_incorrect_icon');
-    
-    ucMlid.multimatch = document.querySelector(mlid).getAttribute("multimatch");
+    if(document.querySelector(mlid)) {
+        ucMlid.multimatch = document.querySelector(mlid).getAttribute("multimatch");
+    }
     if (ucMlid.multimatch < 2) {
         var str = '<svg id="lines"><marker id="triangle" viewBox="0 0 10 10" refX="0" refY="5" markerUnits="strokeWidth" markerWidth="6" markerHeight="5" orient="auto"><path d="M 0 0 L 10 5 L 0 10 z" /></marker>';
         str += '<marker id="trngle" viewBox="0 0 10 10" refX="0" refY="5" markerUnits="strokeWidth" markerWidth="6" markerHeight="5" orient="auto"><path d="M 0 0 L 10 5 L 0 10 z" style="fill:#83C883"/></marker>';
