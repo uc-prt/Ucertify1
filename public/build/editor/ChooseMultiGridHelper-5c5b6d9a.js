78cad8e0604f36327f0bb4cbd47217a5228d5c05
uc-prk <prabhat.kumar@ucertify.com>
2021-11-17 16:58:31 +0530

    updated the code

---
+++ public/build/editor/ChooseMultiGridHelper-5c5b6d9a.js
@@ -0,0 +1,2 @@
+import{S as s,i as e,s as t}from"./main-ebbb55b4.js";const o={helpVideo:{}};export default class extends s{constructor(s){super(),e(this,s,null,null,t,{})}}export{o as itemHelper};
+//# sourceMappingURL=ChooseMultiGridHelper-5c5b6d9a.js.map
---
+++ public/build/editor/defaultXML-7cb754d4.js
@@ -0,0 +1,2 @@
+import{S as e,i as t,s}from"./main-ebbb55b4.js";function a(e){return{"editor_item_69.xml":'<SMXML type="22" addhtml="0" name="Web"></SMXML>',sample:'<SMXML type="22" addhtml="0" name="Web"></SMXML>'}[e]}export default class extends e{constructor(e){super(),t(this,e,null,null,s,{})}}export{a as getDefaultXMl};
+//# sourceMappingURL=defaultXML-7cb754d4.js.map
