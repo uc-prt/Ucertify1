0c37256033c6adf26ca9f7824bb1f2711d7c11dd
uc-prk <prabhat.kumar@ucertify.com>
2021-11-17 23:19:34 +0530

    updating bundles

---
+++ public/build/editor/choiceMatrixHelper-7c7e9c14.js
@@ -0,0 +1,2 @@
+import{S as e,i as s,s as d}from"./main-2c422ec7.js";const c={helpVideo:{0:"//player.vimeo.com/external/286997309.hd.mp4?s=c18e03b856b9446dd2d9dc9ff09b05c7994d2d8d"}};export default class extends e{constructor(e){super(),s(this,e,null,null,d,{})}}export{c as itemHelper};
+//# sourceMappingURL=choiceMatrixHelper-7c7e9c14.js.map
