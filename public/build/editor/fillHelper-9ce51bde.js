740a9b4239a8e1e8f46fc0634737bb3c3cb3138e
uc-prk <prabhat.kumar@ucertify.com>
2021-11-19 21:46:05 +0530

    updated the bundles

--- public/build/editor/fillHelper-9ce51bde.js
+++
@@ -1,2 +0,0 @@
-import{S as s,i as e,s as t}from"./main-5e87a473.js";const o={helpVideo:{}};export default class extends s{constructor(s){super(),e(this,s,null,null,t,{})}}export{o as itemHelper};
-//# sourceMappingURL=fillHelper-9ce51bde.js.map
