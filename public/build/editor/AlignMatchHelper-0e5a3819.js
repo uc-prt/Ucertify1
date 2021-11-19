4906458d7adb7c03baa00d8c7e843a3caa7d6ee5
uc-prk <prabhat.kumar@ucertify.com>
2021-11-19 21:27:11 +0530

    updating bundles

--- public/build/editor/AlignMatchHelper-0e5a3819.js
+++
@@ -1,2 +0,0 @@
-import{S as e,i as a,s as c}from"./main-fcda70bb.js";const s={helpVideo:{0:"//player.vimeo.com/external/289408724.hd.mp4?s=3edaf3922f2c5b983cfc0fcd71594a060603e386",1:"//player.vimeo.com/external/289408724.hd.mp4?s=3edaf3922f2c5b983cfc0fcd71594a060603e386"}};export default class extends e{constructor(e){super(),a(this,e,null,null,c,{})}}export{s as itemHelper};
-//# sourceMappingURL=AlignMatchHelper-0e5a3819.js.map
--- public/build/editor/ChooseMultiGridHelper-8cfcfa29.js
+++
@@ -1,2 +0,0 @@
-import{S as s,i as t,s as e}from"./main-fcda70bb.js";const o={helpVideo:{}};export default class extends s{constructor(s){super(),t(this,s,null,null,e,{})}}export{o as itemHelper};
-//# sourceMappingURL=ChooseMultiGridHelper-8cfcfa29.js.map
