35e04653f34082662261ecf41509d222cda8cd68
uc-prk <prabhat.kumar@ucertify.com>
2021-11-22 14:06:56 +0530

    updating bundles

--- public/build/editor/ChooseMultiGridHelper-26e99bd1.js
+++
@@ -1,2 +0,0 @@
-import{S as s,i as t,s as e}from"./main-c78c4f23.js";const o={helpVideo:{}};export default class extends s{constructor(s){super(),t(this,s,null,null,e,{})}}export{o as itemHelper};
-//# sourceMappingURL=ChooseMultiGridHelper-26e99bd1.js.map
