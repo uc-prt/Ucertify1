09f2972a20710c941036cfa5dc085967df2ebadf
uc-prk <prabhat.kumar@ucertify.com>
2021-11-26 12:27:20 +0530

    updated the code for the ADA

--- public/build/editor/chooseHelper-9b5ca979.js
+++
@@ -1,2 +0,0 @@
-import{S as e,i as s,s as a}from"./main-ce010f0c.js";const f={helpVideo:{0:"//player.vimeo.com/external/287558763.hd.mp4?s=5d5afbce358e88d8fbf9a8dfe0ef80ffa1fd4db6"}};export default class extends e{constructor(e){super(),s(this,e,null,null,a,{})}}export{f as itemHelper};
-//# sourceMappingURL=chooseHelper-9b5ca979.js.map
--- public/build/editor/insightHelper-dfa2d083.js
+++
@@ -1,2 +0,0 @@
-import{S as e,i as a,s as c}from"./main-ce010f0c.js";const s={verticalView:!0,getXmlFunc:"generateXML",helpVideo:"//player.vimeo.com/external/289408469.hd.mp4?s=a7bfff633372e92acdc99c3b9e2e10460ea4ab5c"};export default class extends e{constructor(e){super(),a(this,e,null,null,c,{})}}export{s as itemHelper};
-//# sourceMappingURL=insightHelper-dfa2d083.js.map
