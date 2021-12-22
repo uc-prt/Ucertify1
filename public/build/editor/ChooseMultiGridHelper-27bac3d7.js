cd73f42434ff8c5da24a8f794498fdf467d92a6c
uc-prk <prabhat.kumar@ucertify.com>
2021-12-22 10:19:56 +0530

    updated the code

--- public/build/editor/ChooseMultiGridHelper-27bac3d7.js
+++
@@ -1,2 +0,0 @@
-import{S as s,i as t,s as e}from"./main-2fd9160d.js";const o={helpVideo:{}};export default class extends s{constructor(s){super(),t(this,s,null,null,e,{})}}export{o as itemHelper};
-//# sourceMappingURL=ChooseMultiGridHelper-27bac3d7.js.map
--- public/build/editor/main.js
+++ public/build/editor/main.js
@@ -1,2 +1,2 @@
-export{ah as default}from"./main-2fd9160d.js";
+export{ah as default}from"./main-8dc0502f.js";
 //# sourceMappingURL=main.js.map
