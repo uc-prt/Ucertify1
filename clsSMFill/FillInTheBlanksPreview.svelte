b1a52f71c9513eaf0db3bd2da7797542444305a6
uc-prk <prabhat.kumar@ucertify.com>
2021-12-24 15:14:54 +0530

    updated the code

--- clsSMFill/FillInTheBlanksPreview.svelte
+++ clsSMFill/FillInTheBlanksPreview.svelte
@@ -408,6 +408,7 @@
 	
 	// function calls when remediation mode is on
 	function setReview() {
+		console.trace("YESSS");
 		isReview = true;
 		window.isReview = true;
 		window.learn = true;
@@ -416,25 +417,28 @@
 		state.showToolbar = false;
 		// show the answer and also bind the keys event for ada
 
-		ucFill.modeOn("on");
-		ucFill.showdragans(ajax_eId, 'u', 1);
-		AH.selectAll('.remed_disable', 'show');
-		autoresize(1);
-		//ucFill.modeOn("on");
-		let mathItem = document.getElementById(containerID);
-		mathItem = mathItem ? mathItem.getElementsByClassName('mathquill') : mathItem;
-		if (mathItem) {
-			if(AI.selectAll('.textarea').length > 0) {
-				AI.selectAll('.textarea').forEach((_this)=>{
-					_this.setAttribute('disabled','true');
-				})
-			}
-			AH.selectAll('.fillintheblank','attr',{'disabled':true});
+		
+		setTimeout(function(){
+			ucFill.modeOn("on");
+			ucFill.showdragans(ajax_eId, 'u', 1);
+			AH.selectAll('.remed_disable', 'show');
+			autoresize(1);
+			//ucFill.modeOn("on");
+			let mathItem = document.getElementById(containerID);
+			mathItem = mathItem ? mathItem.getElementsByClassName('mathquill') : mathItem;
+			if (mathItem) {
+				if(AI.selectAll('.textarea').length > 0) {
+					AI.selectAll('.textarea').forEach((_this)=>{
+    					_this.setAttribute('disabled','true');
+					})
+				}
+				AH.selectAll('.fillintheblank','attr',{'disabled':true});
 
-			// AH.setCss(ajax_eId, {"position": "relative"});
-			// AH.insert(ajax_eId, "<div class='spinner-wrapper' style='position:absolute!important;opacity:0!important;'></div>", 'afterbegin');
-		}
-		displayAns();
+				// AH.setCss(ajax_eId, {"position": "relative"});
+				// AH.insert(ajax_eId, "<div class='spinner-wrapper' style='position:absolute!important;opacity:0!important;'></div>", 'afterbegin');
+			}
+			displayAns();
+		}, 50);
 	}
 
 	// function calls when remediation mode is off
