def565a89613a612f47c1c734daaf9e44f633e2b
uc-prk <prabhat.kumar@ucertify.com>
2021-12-27 23:06:58 +0530

    updated the code

--- public/build/editor/fillHelper-e7ed4661.js
+++
@@ -1,2 +0,0 @@
-import{S as s,i as t,s as a}from"./main-42fda00a.js";const e={helpVideo:{}};export default class extends s{constructor(s){super(),t(this,s,null,null,a,{})}}export{e as itemHelper};
-//# sourceMappingURL=fillHelper-e7ed4661.js.map
